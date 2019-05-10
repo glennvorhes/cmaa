7c9843b827665157f5863fdfe2be13f11100b7e9
glennvorhes <gavorhes@wisc.edu>
2019-05-10 16:28:34 -0500

    style and structure updates

--- ts/crashMap.tsx
+++ ts/crashMap.tsx
@@ -1,4 +1,3 @@
-// import {} from 'webmapsjs/dist/reactComponents/reactAndRedux';
 import React = require('react');
 import ReactDom = require('react-dom');
 import ReactRedux = require('react-redux');
@@ -17,6 +16,8 @@ import View from "ol/View";
 import VectorLayer from 'ol/layer/Vector.js';
 import EsriJSON from 'ol/format/EsriJSON';
 import * as lyr from './layers';
+
+import * as constEls from './staticElements';
 
 const esriJson = new EsriJSON();
 
@@ -116,7 +117,16 @@ class _CrashMap extends React.Component<{}, null> {
 
     componentDidMount() {
 
-        $("#accordion").accordion();
+        let $accordion = $('#accordion')
+        $accordion.accordion({heightStyle: 'fill'});
+        // $accordion.accordion();
+
+        // setTimeout(() => {
+        //     $accordion.accordion("resize");
+        //     // jQuery( "#accordion" ).accordion( "resize" );
+        //     // console.log('here');
+        // }, 500)
+
 
         this.map = new Map({
             target: document.getElementById('map'),
@@ -232,99 +242,30 @@ class _CrashMap extends React.Component<{}, null> {
 
     render() {
         return <div id="app-container">
-            <div id="accordion">
-                <h3>Legend</h3>
-                <div>
-                    <img src="/legend.png"/>
+            {constEls.header}
+            <div id="map-container">
+                <div id="accordion-container">
+                    <div id="accordion">
+                        <h3>Legend</h3>
+                        <div>
+                            {/*<img src="/legend.png"/>*/}
+                        </div>
+                        <h3>Selection</h3>
+                        <div>
+                            Selection tools here
+                        </div>
+                        {constEls.disclamerH3}
+                        {constEls.disclamerDiv}
+                        {constEls.aboutH3}
+                        {constEls.aboutDiv}
+                    </div>
                 </div>
-                <h3>Selection</h3>
-                <div>
-                    Selection tools here
+                <div id="map">
+                    <div id="crash-info"/>
+                    <LayerSwitcher mapFunc={() => {
+                        return this.map
+                    }}/>
                 </div>
-                <h3>Disclaimer</h3>
-                <div>
-                    The WISLR crash map is maintained by UW TOPS Lab on behalf of the
-                    Wisconsin Department of Transportation for research and
-                    planning purposes. Any other use, while not prohibited,
-                    is the sole responsibility of the user.
-                    <br/><br/>
-                    Crashes are mapped from 2005 through the current year.
-                    The WISLR crash map is updated on a monthly basis from police
-                    reported crash locations on the DT4000 crash report.
-
-                    <br/><br/>
-                    See the February 2019
-                    <a href="/documents/applications/crash-data/WISLR_Crash_Mapping_Update_201902.pdf" target="_blank">WISLR
-                        Crash Mapping Update</a> summary document for additional information about the crash
-                    mapping data source.
-
-                </div>
-                <h3>About</h3>
-                <div>
-                    <b>About the WisTransPortal Crash Database</b>
-                    <p>
-                        The WisTransPortal system contains a complete database of Wisconsin crash data from 1994 through
-                        the
-                        current year. This database contains information on all police reported crashes in Wisconsin,
-                        including
-                        the location of each crash, vehicles involved, and general crash attributes. Personal data have
-                        been
-                        removed. The TOPS Lab maintains this database for research purposes and as a service to the
-                        Wisconsin
-                        Department of Transportation (WisDOT).
-                    </p>
-
-                    <p><b>"Preliminary" and "Final" Crash Data</b></p>
-                    <p>
-                        Crash records for all police reported crashes from 1994 through the current year are available.
-                        The
-                        WisTransPortal crash database is updated on a nightly basis from extracts provided by WisDOT
-                        Division of State Patrol (DSP) Bureau of Transportation Safety (BOTS). The database includes
-                        both "preliminary" and "final year" crash data. Preliminary data represents the latest set of
-                        available crash records for the current year and generally includes crash reports transmitted by
-                        law enforcement as recently as the previous day. All preliminary data are subject to ongoing
-                        review and editing and may not be suitable for analysis purposes. Final year data represents
-                        the official closed WisDOT crash file for a given year.
-                    </p>
-
-                    <p><b>Wisconsin "Reportable" Crashes</b></p>
-                    <p>
-                        A reportable crash is defined as a crash resulting in injury or death of any person, any damage
-                        to
-                        government-owned non-vehicle property to an apparent extent of $200 or more, or total damage to
-                        property owned by any one person to an apparent extent of $1000 or more. (This definition went
-                        into
-                        effect 1/1/96). It is important to note, however, that not all reportable crashes are reported.
-                        In
-                        order for a crash to be in the database, a crash report must have been completed by a police
-                        officer.
-                    </p>
-
-                    <p><b>Differences Between WisTransPortal and WisDOT Crash Statistics</b></p>
-
-                    This facility provides a user interface to query and retrieve crash records based on high level
-                    attributes such as date range, location, and first harmful event. Often there may be more than one
-                    way to formulate a query selection, especially for intersection based queries. <b>Based on the
-                    particular search criteria, your results may differ from official crash counts reported by
-                    WisDOT.</b>
-
-
-                </div>
-            </div>
-            <div id="map" style={{position: 'relative'}}>
-                <div id='crash-info' style={{
-                    position: 'absolute',
-                    right: '10px',
-                    top: '10px',
-                    zIndex: 5,
-                    height: '200px',
-                    width: '300px',
-                    backgroundColor: 'white',
-                    overflow: 'auto'
-                }}/>
-                <LayerSwitcher mapFunc={() => {
-                    return this.map
-                }}/>
             </div>
         </div>
     }
